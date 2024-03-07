17b0106519f17c0bc8ba35333e949a910034797d
haniff <mohamad.h@hotmail.no>
2024-03-07 22:28:50 +0100

    fill fraction of timer

--- Quiz Master/Assets/Scripts/Timer.cs
+++ Quiz Master/Assets/Scripts/Timer.cs
@@ -6,13 +6,39 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectQuestion = 10f;
+    public bool isAnsweringQuestion = false;
+    public float fillFraction;
     float timerValue;
     void Update()
     {
-
+        UpdateTimer();
     }
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
+        if (isAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectQuestion;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
     }
 }
